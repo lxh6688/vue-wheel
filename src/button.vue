92c26e6464cbc02af22f3f9d73c64995c7530531
codetiger <1272662790@qq.com>
2020-09-11 15:34:05 +0800

    添加 icon 和 iconPosition  属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,16 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
 export default {
-
+    props: ['icon', 'iconPosition']
 }
 </script>
 <style lang="scss">
@@ -14,6 +21,10 @@ export default {
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover{
         border-color: var(--border-color-hover);
     }
@@ -22,6 +33,23 @@ export default {
     }
     &:focus{
         outline: none;
+    }
+    > .content{
+        order: 2;
+    }
+    > .icon{
+        order: 1;
+        margin-right: .1em;
+    }
+    
+    &.icon-right{
+        > .content{
+            order: 1;
+        }
+        > .icon{
+            order: 2;
+            margin-left: .1em;
+        }
     }
 }
 </style>
