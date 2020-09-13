9f0391cf02773d1379de0c52414680ace9c1b091
codetiger <1272662790@qq.com>
2020-09-13 23:20:58 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
         <g-icon class="icon" v-if="icon" :name='icon'></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot/>
         </div>
@@ -22,6 +23,10 @@ export default {
 }
 </script>
 <style lang="scss">
+@keyframes spin{
+    0%{ transform: rotate(0deg); }
+    100%{ transform: rotate(360deg); }
+}
 .g-button{
     font-size: var(--font-size);
     height: var(--button-height);
@@ -43,6 +48,9 @@ export default {
         > .content{ order: 1; }
         > .icon{ order: 2; margin-left: .1em; }
     }
+}
+.loading{
+    animation: spin 2s infinite linear;
 }
 </style>
 
